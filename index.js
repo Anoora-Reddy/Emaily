e2a5951fe70e3f9c812dac1c320a8069027104ce
Anoora-Reddy <vappidi@uab.edu>
2024-08-29 15:34:47 -0500

    First Comment

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/anoora', (req, res) => {
+    res.send({
+        hi:'there'
+    });
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
