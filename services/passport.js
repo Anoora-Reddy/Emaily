404a36c222aec357e7a61ba39a7ba858f72cf122
Anoora-Reddy <vappidi@uab.edu>
2024-09-05 12:57:32 -0500

    Updated Passport

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true // not sure whether it is secure or not
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
